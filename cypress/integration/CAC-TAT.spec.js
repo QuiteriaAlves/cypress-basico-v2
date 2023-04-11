1658ec9c2adb1a8be23a89a364136adb969ce612
Quiteria <qteixeira@yahoo.com.br>
2023-04-11 12:28:12 -0300

    New create cypress project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -279,7 +279,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
             })
     })
 
-    it.only('encontra o gato escondido', () => {
+    it('encontra o gato escondido', () => {
         cy.get('#cat')
             .invoke('show')
             .should('be.visible')
